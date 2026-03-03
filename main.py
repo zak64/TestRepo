8bcdb453460896e25a74966f8d3fde2804a79a6b
zak64 <zak64@users.noreply.github.com>
2026-03-03 14:24:25 -0500

    Uncomment exception handling in function f1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 
 def f1(a):
-    # if a == 0: 
-    #     raise Exception(f"JZ Raise Exception, inoput a not valid is 0")
-    # return a
-    return 3 # break the unit test
+    if a == 0: 
+         raise Exception(f"JZ Raise Exception, inoput a not valid is 0")
+    return a
+    #return 3 # break the unit test
 
 
